d38921f4dae0c08a1bab4cb42f839669ec8334b0
Davi <davii.clink@gmail.com>
2021-10-10 19:52:23 -0300

    feito

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+
+import './App.css'
+
+
+import Teste from "./tristeza/teste";
+import Contador from "./tristeza/comunicacaocomps/contador";
+import Pedido from "./tristeza/comunicacaocomps/finalizarPedido/pedido";
+import ConclusaoPedido from "./tristeza/comunicacaocomps/finalizarPedido/conclusao";
+import App from './tristeza/home'
+import DetalheProduto from './tristeza/detalheproduto'
+import Carrinho from './tristeza/carrinho/carrinho-item'
+
+
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={App} />
+        <Route path="/detalhe" component={DetalheProduto} />
+        <Route path="/carrinho" component={Carrinho} />
+        
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+        <Route path="/teste" component={Teste} />
+        <Route path="/infoX/contador" exact={true} component={Contador} />
+        <Route path="/infoX/pedido" component={Pedido} />
+        <Route path="/infoX/conclusao" component={ConclusaoPedido} />
+
+      </Switch>
+    </BrowserRouter>
+  </StrictMode>,
+  rootElement
+);
